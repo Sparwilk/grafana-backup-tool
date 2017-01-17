424719a3023168f2632af7d4c3ab3b483891a6e8
Ryan Craig <ryan.craig@smartbox.com>
2017-01-17 12:03:40 +0000

    Refactoring and adding the ability to save datasources

--- pushToElasticsearch.py
+++ pushToElasticsearch.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
 import json, requests, optparse, os, sys, datetime
 
-def get_all_dashboards(grafana_url, headers):
+def GET_ALL_DATASOURCES(grafana_url, headers):
+    r = requests.get(grafana_url+"/api/datasources/", headers=headers)
+    return json.loads(r.content)
+
+def GET_ALL_DASHBOARDS(grafana_url, headers):
     r = requests.get(grafana_url+"/api/search/", headers=headers)
     return json.loads(r.content)
 
-def get_individual_dashboard(dashboard_uri, grafana_url, headers):
+def GET_INDIVIDUAL_DASHBOARD(dashboard_uri, grafana_url, headers):
     r = requests.get(grafana_url + "/api/dashboards/" + dashboard_uri, headers=headers)
     return r.content
 
-def create_index_if_missing(elasticsearch_url,index_name):
+def CREATE_INDEX_IF_MISSING(elasticsearch_url,index_name):
     index_url = elasticsearch_url + "/" + index_name
     r = requests.head(index_url)
     mappings = json.dumps({"settings": {"index.mapping.ignore_malformed": True }})
@@ -23,8 +27,8 @@ def create_index_if_missing(elasticsearch_url,index_name):
         print r.text
         return ""
 
-def save_dashboard_to_elasticsearch(dashboard_url, json_data):
-    r = requests.put(dashboard_url, data=json_data)
+def SAVE_TO_ELASTICSEARCH(document_url, json_data):
+    r = requests.put(document_url, data=json_data)
     print json.dumps(r.json())
     return r.status_code
 
@@ -69,6 +73,12 @@ parser.add_option('-c', '--cron',
     help    = 'Generate an automatic index name based on the default value + the current date',
     action  = 'store_true')
 
+parser.add_option('-d', '--datasources',
+    default = False,
+    dest    = 'save_datasources',
+    help    = 'Choose whether to save the datasources as well',
+    action  = 'store_true')
+
 (options, args) = parser.parse_args()
 
 if not options.elastic_host:
@@ -81,29 +91,39 @@ elastic_port = options.elastic_port
 grafana_host = options.grafana_host
 grafana_port = options.grafana_port
 grafana_api_key = options.grafana_api_key
+
 if options.run_as_cron:
     index_name = options.index_name + "-" + datetime.date.today().strftime('%Y.%m.%d')
 else:
     index_name = options.index_name
 
-http_get_headers = {'Authorization': 'Bearer ' + grafana_api_key}
-http_post_headers = {'Authorization': 'Bearer ' + grafana_api_key, 'Content-Type': 'application/json'}
-
+http_headers = {'Authorization': 'Bearer ' + grafana_api_key, 'Content-Type': 'application/json'}
 elasticsearch_url = "http://" + elastic_host + ":" + elastic_port 
 grafana_url = "http://" + grafana_host + ":" + grafana_port
 
 #Creates the index on ES if it doesn't already exist
-index_url = create_index_if_missing(elasticsearch_url, index_name)
-
-all_dashboards = get_all_dashboards(grafana_url, http_get_headers)
+index_url = CREATE_INDEX_IF_MISSING(elasticsearch_url, index_name)
+dashboard_list = GET_ALL_DASHBOARDS(grafana_url, http_headers)
 
 if not index_url:
     print "Unable to create index: " + index_url
     sys.exit(1)
 
-for dashboard in all_dashboards:
-    data = get_individual_dashboard(dashboard['uri'], grafana_url, http_get_headers)
-    grafana_exported_dashboard = json.loads(data)
+if options.save_datasources:
+    datasource_list = GET_ALL_DATASOURCES(grafana_url, http_headers)
+    for datasource in datasource_list:
+        elasticsearch_format_datasource = {
+                "group": "guest",
+                "tags": [],
+                "user": "guest",
+                "title": datasource['name'],
+                "datasource" : json.dumps(datasource)
+                }
+        full_datasource_url = index_url + "/datasource/" + datasource['name']
+        SAVE_TO_ELASTICSEARCH(full_datasource_url, json.dumps(elasticsearch_format_datasource))
+
+for dashboard in dashboard_list:
+    grafana_exported_dashboard = json.loads(GET_INDIVIDUAL_DASHBOARD(dashboard['uri'], grafana_url, http_headers))
     grafana_exported_dashboard['dashboard']['id'] = None
     #Set the ID to none so that Grafana can assign it's own index when importing
     elasticsearch_format_dashboard = {
@@ -116,5 +136,5 @@ for dashboard in all_dashboards:
     #slug is URL compatible dashboard name
     elasticsearch_slug = grafana_exported_dashboard['meta']['slug']
     full_dashboard_url = index_url + "/dashboard/" + elasticsearch_slug
-    save_dashboard_to_elasticsearch(full_dashboard_url, json.dumps(elasticsearch_format_dashboard))
+    SAVE_TO_ELASTICSEARCH(full_dashboard_url, json.dumps(elasticsearch_format_dashboard))
 sys.exit(0)
